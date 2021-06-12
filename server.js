f25d3d1a918c9e5f9887e42d662388566db043dd
Von Divino <divinovon@gmail.com>
2021-06-12 10:48:03 +0800

    added sailor

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const buoyr = express();
 const editor = express();
+const sailor = express();
 
 editor.use(express.static(__dirname+'/editor/build'));
+sailor.use(express.static(__dirname+'/sailor/build'));
 
 buoyr.get('/', (req, res) => {
     res.send('Hello there, BuoyrTech!');
@@ -12,6 +14,11 @@ editor.get('*', (req, res) => {
     res.sendFile(__dirname+'/editor/build/'+'index.html');
 });
 
+sailor.get('*', (req, res) => {
+    res.sendFile(__dirname+'/sailor/build/'+'index.html');
+});
+
 
 buoyr.listen(8080);
 editor.listen(8081);
+sailor.listen(8082);
