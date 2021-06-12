711ffa8c112bfe725c8e8d4f6328a3d710837a6e
Von Divino <divinovon@gmail.com>
2021-06-12 11:25:37 +0800

    prepare anchor for flutter dev

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const buoyr = express();
 const editor = express();
 const sailor = express();
+const anchor = express();
 
 editor.use(express.static(__dirname+'/editor/build'));
 sailor.use(express.static(__dirname+'/sailor/build'));
+anchor.use(express.static(__dirname+'/anchor/build/web'));
 
 buoyr.get('/', (req, res) => {
     res.send('Hello there, BuoyrTech!');
@@ -18,7 +20,12 @@ sailor.get('*', (req, res) => {
     res.sendFile(__dirname+'/sailor/build/'+'index.html');
 });
 
+anchor.get('*', (req, res) => {
+    res.sendFile(__dirname+'/anchor/build/web/'+'index.html');
+});
+
 
 buoyr.listen(8080);
 editor.listen(8081);
 sailor.listen(8082);
+anchor.listen(8083);
